e3b78b6d50e7cffd190782ca3574f801b1fc582b
sagar rajak <sagarrajak858@gmail.com>
2020-06-13 19:07:18 +0530

    card scss template added

--- src/component/foodCard/FoodCardComponent.jsx
+++ src/component/foodCard/FoodCardComponent.jsx
@@ -1,32 +1,30 @@
 import React from "react";
-import {
-  Button,
-  Card,
-  CardBody,
-  CardImg,
-  CardSubtitle,
-  CardText,
-  CardTitle,
-} from "reactstrap";
+import { Button, Card, CardBody, CardFooter, CardImg, CardSubtitle, CardText, CardTitle } from "reactstrap";
+import './FoodCardComponent.scss';
+
 
 export function FoodCardComponent() {
   return (
-    <Card>
+    <Card className="foodCard">
       <CardImg
         top
         width="100%"
         src="/assets/318x180.svg"
         alt="Card image cap"
+        className="foodCard__cardImage"
       />
-      <CardBody>
-        <CardTitle>Card title</CardTitle>
-        <CardSubtitle>Card subtitle</CardSubtitle>
-        <CardText>
+      <CardBody className="foodCard__cardBody">
+        <CardTitle className="foodCard__cardBody-cardTitle">Card title</CardTitle>
+        <CardSubtitle className="foodCard__cardBody-cardSubtitle">Card subtitle</CardSubtitle>
+        <CardText className="foodCard__cardBody-cardText">
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </CardText>
-        <Button>Button</Button>
       </CardBody>
+      <CardFooter className="foodCard__cardFooter">
+         <Button className="foodCard__cardFooter-callButton">Call</Button>
+         <Button className="foodCard__cardFooter-deleteButton">Delete</Button>
+      </CardFooter>
     </Card>
   );
 }
